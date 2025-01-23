8d68582f955ce227d9342daeef61dfc9d10cd743
maxzuffer <mzuffer.ieu2023@student.ie.edu>
2025-01-23 17:12:35 +0100

    Adding another if statement did not work for me

---
+++ A few lines.py
@@ -0,0 +1,7 @@
+grade = int(input("Which grade will I receive in this class?"))
+print(grade)
+if grade == 10:
+    print("A benevolent angel appears and gives the professor good luck until the end of time")
+else:
+    print("An evil demon appears and courses the professor for 10 years")
+
